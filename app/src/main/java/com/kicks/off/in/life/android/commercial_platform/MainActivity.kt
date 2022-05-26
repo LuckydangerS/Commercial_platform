3b462993eec1fb4931e02ff8bf5c855616e791b4
LuckydangerS <kicks.off.in.life@gmail.com>
2022-05-26 17:34:30 +0400

    Change views

--- app/src/main/java/com/kicks/off/in/life/android/commercial_platform/MainActivity.kt
+++ app/src/main/java/com/kicks/off/in/life/android/commercial_platform/MainActivity.kt
@@ -2,10 +2,47 @@ package com.kicks.off.`in`.life.android.commercial_platform
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.MenuItem
+import android.widget.Toast
+import androidx.appcompat.app.ActionBarDrawerToggle
+import androidx.core.view.GravityCompat
+import com.google.android.material.navigation.NavigationView
+import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.main_content.*
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelectedListener {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        init()
+    }
+
+    private fun init(){
+
+        val toggle = ActionBarDrawerToggle(this, drawerLayout,toolbar,
+            R.string.open, R.string.close)
+        drawerLayout.addDrawerListener(toggle)
+        toggle.syncState()
+        navView.setNavigationItemSelectedListener(this)
+
+    }
+
+    override fun onNavigationItemSelected(item: MenuItem): Boolean {
+
+        when(item.itemId){
+
+            R.id.id_my_ads -> {
+                Toast.makeText(this, "Presed id_my_ads", Toast.LENGTH_LONG).show()
+            }
+            R.id.id_car -> {}
+            R.id.id_pc -> {}
+            R.id.id_dm -> {}
+            R.id.id_sign_up -> {}
+            R.id.id_sign_in -> {}
+            R.id.id_sign_out -> {}
+
+        }
+        drawerLayout.closeDrawer(GravityCompat.START)
+        return true
     }
 }
