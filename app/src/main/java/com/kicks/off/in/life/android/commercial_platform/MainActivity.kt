e9d04a325a16aec73cdefbf2e09e358f5698e2f2
LuckydangerS <kicks.off.in.life@gmail.com>
2022-05-26 12:58:20 +0400

    Creating project

---
+++ app/src/main/java/com/kicks/off/in/life/android/commercial_platform/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.kicks.off.`in`.life.android.commercial_platform
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
