ce7d5ae6e9619fc91986ef567ae1c1e29f112d9c
Ambush_blitZ <kicks.off.in.life@gmail.com>
2022-05-30 18:33:54 +0400

    AuthConst

---
+++ app/src/main/java/com/kicks/off/in/life/android/commercial_platform/constans/FirebaseAuthConstans.kt
@@ -0,0 +1,6 @@
+package com.kicks.off.`in`.life.android.commercial_platform.constans
+
+object FirebaseAuthConstans {
+    const val ERROR_EMAIL_ALREADY_IN_USE = "ERROR_EMAIL_ALREADY_IN_USE"
+    const val ERROR_INVALID_EMAIL = "ERROR_INVALID_EMAIL"
+}
