b9e1702ef0e67ccd1e0237baf27dc09e80e8884d
Ambush_blitZ <kicks.off.in.life@gmail.com>
2022-06-02 21:48:16 +0400

    fix trabls, SDK 28

--- app/src/main/java/com/kicks/off/in/life/android/commercial_platform/act/EditAdsAct.kt
+++ app/src/main/java/com/kicks/off/in/life/android/commercial_platform/act/EditAdsAct.kt
@@ -4,19 +4,23 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.ArrayAdapter
 import com.kicks.off.`in`.life.android.commercial_platform.databinding.ActivityEditAdsBinding
+import com.kicks.off.`in`.life.android.commercial_platform.dialogs.DialogSpinnerHelper
 import com.kicks.off.`in`.life.android.commercial_platform.utils.CityHelper
 
 class EditAdsAct : AppCompatActivity() {
     private lateinit var rootElement:ActivityEditAdsBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         rootElement = ActivityEditAdsBinding.inflate(layoutInflater)
         setContentView(rootElement.root)
 
-        val adapter = ArrayAdapter(
-            this, android.R.layout.simple_spinner_item,
-            CityHelper.getAllCountries(this))
-        adapter.setDropDownViewResource(android.R.layout.simple_spinner_item)
-        rootElement.spCountry.adapter = adapter
+        val listCountry = CityHelper.getAllCountries(this)
+        val dialog = DialogSpinnerHelper()
+        dialog.showSpinnerDialog(this, listCountry)
     }
+
+    //private fun init(){}
+
+
 }
---
+++ app/src/main/java/com/kicks/off/in/life/android/commercial_platform/dialogs/DialogSpinnerHelper.kt
@@ -0,0 +1,25 @@
+package com.kicks.off.`in`.life.android.commercial_platform.dialogs
+
+import android.app.AlertDialog
+import android.content.Context
+import android.view.LayoutInflater
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.kicks.off.`in`.life.android.commercial_platform.R
+
+class DialogSpinnerHelper {
+
+    fun showSpinnerDialog(context: Context,list: ArrayList<String>){
+        val builder = AlertDialog.Builder(context)
+        val rootView = LayoutInflater.from(context).inflate(R.layout.spinner_layout, null)
+        val adapter = RcViewDialogSpinner()
+        val rcView = rootView.findViewById<RecyclerView>(R.id.rcSpView)
+        rcView.layoutManager = LinearLayoutManager(context)
+        rcView.adapter = adapter
+
+        builder.setView(rootView)
+        adapter.updateAdapter(list)
+        builder.show()
+
+    }
+}
