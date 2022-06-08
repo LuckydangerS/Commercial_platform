7bba2d9a33ccd1f710bec85c651040643115646c
Ambush_blitZ <kicks.off.in.life@gmail.com>
2022-06-08 16:14:55 +0400

    "toLowerCase" on the "lowercase"

--- app/src/main/java/com/kicks/off/in/life/android/commercial_platform/utils/CityHelper.kt
+++ app/src/main/java/com/kicks/off/in/life/android/commercial_platform/utils/CityHelper.kt
@@ -40,7 +40,7 @@ object CityHelper {
             return tempList
         }
         for (selection: String in list) {
-            if (selection.toLowerCase(Locale.ROOT).startsWith(searchText.toLowerCase(Locale.ROOT)))
+            if (selection.lowercase(Locale.ROOT).startsWith(searchText.lowercase(Locale.ROOT)))
                 tempList.add(selection)
         }
         if (tempList.size == 0)tempList.add("No result")
