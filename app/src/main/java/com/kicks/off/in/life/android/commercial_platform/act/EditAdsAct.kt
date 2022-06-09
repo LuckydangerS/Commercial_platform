e88c1594e40ddd68a9833f7d32c9e0e78d67dcf1
Ambush_blitZ <kicks.off.in.life@gmail.com>
2022-06-09 12:36:23 +0400

    camera TRABL!!!!

--- app/src/main/java/com/kicks/off/in/life/android/commercial_platform/act/EditAdsAct.kt
+++ app/src/main/java/com/kicks/off/in/life/android/commercial_platform/act/EditAdsAct.kt
@@ -1,17 +1,24 @@
 package com.kicks.off.`in`.life.android.commercial_platform.act
 
+import android.content.Intent
+import android.content.pm.PackageManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.view.View
 import android.widget.Toast
+import com.fxn.pix.Pix
+import com.fxn.utility.PermUtil
 import com.kicks.off.`in`.life.android.commercial_platform.R
 import com.kicks.off.`in`.life.android.commercial_platform.databinding.ActivityEditAdsBinding
 import com.kicks.off.`in`.life.android.commercial_platform.dialogs.DialogSpinnerHelper
 import com.kicks.off.`in`.life.android.commercial_platform.utils.CityHelper
+import com.kicks.off.`in`.life.android.commercial_platform.utils.ImagePicker
 
 class EditAdsAct : AppCompatActivity() {
     lateinit var binding:ActivityEditAdsBinding
     private var dialog = DialogSpinnerHelper()
+    //private var isImagesPermissionGranted = false
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -19,6 +26,40 @@ class EditAdsAct : AppCompatActivity() {
         setContentView(binding.root)
         init()
 
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (resultCode == RESULT_OK && requestCode == ImagePicker.REQUEST_CODE_GET_IMAGES) {
+            if (data != null){
+                val returnValue = data.getStringArrayListExtra(Pix.IMAGE_RESULTS)
+                Log.d("MyLog", "Image :${returnValue?.get(0)}")
+                Log.d("MyLog", "Image :${returnValue?.get(1)}")
+                Log.d("MyLog", "Image :${returnValue?.get(2)}")
+
+            }
+        }
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        when (requestCode) {
+            PermUtil.REQUEST_CODE_ASK_MULTIPLE_PERMISSIONS -> {
+                if (grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED){
+                    ImagePicker.getImages(this)
+                } else {
+                //    isImagesPermissionGranted = false
+                    Toast.makeText(this, "Apppppp", Toast.LENGTH_LONG).show()
+
+                }
+                return
+
+            }
+        }
     }
 
     private fun init(){
@@ -45,5 +86,9 @@ class EditAdsAct : AppCompatActivity() {
 
 
     }
+    fun onClickGetImages(view: View){
+        ImagePicker.getImages(this)
 
+
+    }
 }
---
+++ app/src/main/java/com/kicks/off/in/life/android/commercial_platform/utils/ImagePicker.kt
@@ -0,0 +1,60 @@
+package com.kicks.off.`in`.life.android.commercial_platform.utils
+
+import androidx.appcompat.app.AppCompatActivity
+import com.fxn.pix.Options
+import com.fxn.pix.Pix
+
+
+object ImagePicker {
+    const val REQUEST_CODE_GET_IMAGES = 999
+    fun getImages(context: AppCompatActivity){
+        val options = Options.init()
+            .setRequestCode(REQUEST_CODE_GET_IMAGES)
+            .setCount(3)
+            .setFrontfacing(false)
+            .setMode(Options.Mode.Picture)
+            .setScreenOrientation(Options.SCREEN_ORIENTATION_PORTRAIT)
+            .setPath("/pix/images")
+
+        Pix.start(context, options)
+    }
+
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+/*
+
+
+    private fun getOptions(imageCounter : Int) : BitmapFactory.Options {
+        val options = BitmapFactory.Options().apply {
+           // ratio = Ratio.RATIO_AUTO                                    //Image/video capture ratio
+            count = imageCounter
+            isFrontFacing =  false
+            mode = Mode.Picture
+            path = "Pix/Camera"
+        }
+        return options
+    }
+
+
+    fun getImages(context: AppCompatActivity){
+
+        }
+        addPixToActivity(R.id.container, options) {
+            when (it.status) {
+                PixEventCallback.Status.SUCCESS -> //use results as it.data
+                    PixEventCallback.Status.BACK_PRESSED -> // back pressed called
+            }
+        }
+*/
