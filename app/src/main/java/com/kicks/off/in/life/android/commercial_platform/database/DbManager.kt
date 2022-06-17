76d1f6ace357fdf43c3138719ee6bb38b505de3f
Ambush_blitZ <kicks.off.in.life@gmail.com>
2022-06-17 23:29:46 +0400

    DbManager

--- app/src/main/java/com/kicks/off/in/life/android/commercial_platform/database/DbManager.kt
+++ app/src/main/java/com/kicks/off/in/life/android/commercial_platform/database/DbManager.kt
@@ -4,7 +4,7 @@ import com.google.firebase.database.ktx.database
 import com.google.firebase.ktx.Firebase
 
 class DbManager {
-    private val db = Firebase.database.reference
+    private val db = Firebase.database.getReference("main")
 
     fun publishAd(){
 
